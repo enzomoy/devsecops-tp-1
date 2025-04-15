ea869d5a0f2cf5581c8d13c89cd14d1cb6a1a688
Enzo Moy <enzo.moy@affluences.com>
2025-04-15 14:33:31 +0200

    add tests

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,13 @@ import { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
-// Middleware de base
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Route de base
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-// Démarrage du serveur
 app.listen(port, () => {
     console.log(`Serveur démarré sur http://localhost:${port}`);
 });
